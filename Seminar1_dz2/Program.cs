33872baa32b119a57cb8a52c4b7af9be521a6f97
Andrusha <andruwaeinne1@gmail.com>
2023-09-15 18:22:38 +0700

    dz2

---
+++ Seminar1_dz2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа и 
+// выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число  :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число  :");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число  :");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int result = num1;
+
+
+if (num2 > result) result = num2;
+if (num3 > result) result = num3;
+
+Console.WriteLine ($"Наибольшим числом является : {result}");
