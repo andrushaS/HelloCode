5d10231b7bceb6c388acf59fef7873266b341a5e
Andrusha <andruwaeinne1@gmail.com>
2023-09-15 17:03:25 +0700

    Metod

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 15, 32, 32, 18, 45, 36, 27, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+}
+
