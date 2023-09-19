b878a20ac0a0e3ca875fdb12bd2c79d91f91667e
Andrusha <andruwaeinne1@gmail.com>
2023-09-19 23:48:07 +0700

    dz13

---
+++ Seminar2_dz13/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая выводит третью цифру заданного 
+//числа или сообщает, что третьей цифры нет.
+
+
+
+void CutNumber (int num=0)
+{
+    while (num>999)
+    {
+        num/=10;
+    }
+    if (num<99)
+    {
+        Console.WriteLine("Третей цифры нет!");
+    }
+    else
+    {;
+        int lastdigit = num%10;
+        int newnumber = lastdigit; 
+        Console.WriteLine($"New number is {newnumber}");
+    }
+}
+   
+Console.WriteLine("Enter number:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Your number is {number}");
+CutNumber(number);
+
