2404be35fc77f0ce71ba77ca1ecc7ec885e63207
Andrusha <andruwaeinne1@gmail.com>
2023-09-19 19:08:02 +0700

    xample13

---
+++ Example013_FOR_WHILE/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string Method4(int count, string text)
+{
+    
+    string result = string.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string result = Method4(10, "z");
+Console.WriteLine(result);
