c3323110144543d0976955548c5e2985c14a6f88
Andrusha <andruwaeinne1@gmail.com>
2023-09-27 16:00:40 +0700

    seminar3_dz1

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -83,17 +83,41 @@
 // принимает на вход число (N) и выдаёт таблицу
 // квадратов чисел от 1 до N.
 
-void square(int num)
-{
-    for(int i= 1; i<=num;i++)
-    {
+// void square(int num)
+// {
+//     for(int i= 1; i<=num;i++)
+//     {
 
-        Console.Write($"{i*i} ");
-    }
+//         Console.Write($"{i*i} ");
+//     }
+// }
+// Console.WriteLine("Enter a number: ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// if (numb<1)
+//     Console.WriteLine("Wrong nomber");
+// else 
+//     square (numb);
+
+void Priverka (int num = 0, int num2 = 0)
+{
+    int firstdigit = num/100;
+    int rev = num%10 * 10000 + (num/10)%10 * 1000 + (num/100)%10 * 100 + (num/1000)%10 * 10 + num/10000;
+    int lastdigit = rev/100;
+         
+        if (firstdigit == lastdigit)
+        {
+        Console.WriteLine($"True");
+        }
+        else if (num <9999 || num >99999)
+        Console.WriteLine($"Число не пятизначное");
+        else 
+        Console.WriteLine($"False");
 }
+
 Console.WriteLine("Enter a number: ");
 int numb = Convert.ToInt32(Console.ReadLine());
-if (numb<1)
-    Console.WriteLine("Wrong nomber");
-else 
-    square (numb);
+Priverka(numb);
+
+
+// void Priverka (int num = 0; temp = 0; rev =0);
+// {
---
+++ Seminar3_dz1/Program.cs
@@ -0,0 +1,20 @@
+﻿void Priverka (int num = 0, int num2 = 0)
+{
+    int firstdigit = num/100;
+    int rev = num%10 * 10000 + (num/10)%10 * 1000 + (num/100)%10 * 100 + (num/1000)%10 * 10 + num/10000;
+    int lastdigit = rev/100;
+         
+        if (firstdigit == lastdigit)
+        {
+        Console.WriteLine($"True");
+        }
+        else if (num <9999 || num >99999)
+        Console.WriteLine($"Число не пятизначное");
+        else 
+        Console.WriteLine($"False");
+}
+
+Console.WriteLine("Enter a number: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+Priverka(numb);
+
