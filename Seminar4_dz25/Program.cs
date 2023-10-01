46ef666d7a21c7d1546370b61652ff2defcd23c3
Andrusha <andruwaeinne1@gmail.com>
2023-10-01 16:44:49 +0700

    seminar4_dz25

---
+++ Seminar4_dz25/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и
+// возводит число A в натуральную степень B.
+
+int lowTO(int firstNum, int secondNum)
+{
+  int res = 1;
+  for(int i=1; i <= secondNum; i++){
+    res = res * firstNum;
+  }
+    
+    return res;
+}
+
+  Console.Write("Enter first number: ");
+  int firstNum = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Enter second number: ");
+  int secondNum = Convert.ToInt32(Console.ReadLine());
+
+  int result = lowTO(firstNum, secondNum);
+  Console.WriteLine($"Your number is : {result}");
+
