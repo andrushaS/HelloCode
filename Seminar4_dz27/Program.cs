f093f86e2df142d1bbd00d6883e296904ce6d479
Andrusha <andruwaeinne1@gmail.com>
2023-10-01 17:22:17 +0700

    seminar4_dz27

---
+++ Seminar4_dz27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и 
+//выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void summa (int num)
+{
+    int res =0;
+    while (num > 0)
+    {
+     res += num%10;
+     num = num/10;
+    }
+     Console.WriteLine($"New number is {res}");
+    
+}
+
+Console.WriteLine("Please enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+summa(num);
