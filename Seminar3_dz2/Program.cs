617d9db1fe582a2cf5d62a7cb9200ad5d1f0a556
Andrusha <andruwaeinne1@gmail.com>
2023-10-01 16:17:17 +0700

    seminar4

--- Seminar3_dz2/Program.cs
+++ Seminar3_dz2/Program.cs
@@ -1,27 +1,67 @@
-﻿//Внутри класса Answer напишите метод DistanceBetweenPoints, который при
-// нимает на вход координаты двух точек pointA и pointB в виде масси
-// ва целых чисел, и возвращает расстояние между ними в 3D пространстве.
+﻿// //Внутри класса Answer напишите метод DistanceBetweenPoints, который при
+// // нимает на вход координаты двух точек pointA и pointB в виде масси
+// // ва целых чисел, и возвращает расстояние между ними в 3D пространстве.
 
 
 
 
-Console.WriteLine("Enter x1 point: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter y1 point: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter z1 point: ");
-int x3 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter x2 point: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter y2 point: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter z2 point: ");
-int y3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter x1 point: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter y1 point: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter z1 point: ");
+// int x3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter x2 point: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter y2 point: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter z2 point: ");
+// int y3 = Convert.ToInt32(Console.ReadLine());
 
-int A = y1 - x1;
-int B = y2 - x2;
-int C = y3 - x3;
+// int A = y1 - x1;
+// int B = y2 - x2;
+// int C = y3 - x3;
 
-double length = Math.Round(Math.Sqrt(A * A + B * B + C * C),2);
-Console.WriteLine("Расстояние = " + length);
+// double length = Math.Round(Math.Sqrt(A * A + B * B + C * C),2);
+// Console.WriteLine("Расстояние = " + length);
 
+using System;
+
+public class Answer
+{
+    private static double Length(int[] pointA, int[] pointB)
+    {
+  int A = y1 - x1;
+  int B = y2 - x2;
+  int C = y3 - x3;
+  return
+  Length = Math.Round(Math.Sqrt(A * A + B * B + C * C),2);
+
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args) {
+        int x1, x2, x3, y1, y2, y3;
+
+        if (args.Length >= 6) {
+            x1 = int.Parse(args[0]);
+            x2 = int.Parse(args[1]);
+            x3 = int.Parse(args[2]);
+            y1 = int.Parse(args[3]);
+            y2 = int.Parse(args[4]);
+            y3 = int.Parse(args[5]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            x1 = 3;
+            x2 = 6;
+            x3 = 8;
+            y1 = 2;
+            y2 = 1;
+            y3 = -7;
+        }
+
+        // Не удаляйте строки ниже
+        double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+        Console.WriteLine($"{result:F2}");
+    }
+}
---
+++ Seminar4/Program.cs
@@ -0,0 +1,127 @@
+﻿// Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+
+// void SummNumb (int numb)
+// {
+//     int res = 0;
+//     for (int i=1; i<=numb; i++)
+//     {
+//         res+=i;
+//     }
+//     Console.WriteLine($"Your sum is form 1 to {numb} is {res}");
+// }
+// Console.WriteLine("Please enter your number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// SummNumb(num);
+
+// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// void NumInDec (int num)
+// {
+//     int count = 0;
+//     while (num !=0 )
+//     {
+//         num /= 10;
+//         count++;
+//     }
+//     Console.WriteLine ($"Count of number is {count}");
+// }
+// Console.WriteLine("Please enter your number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// NumInDec(num);
+
+// Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+// void Fact (int n)
+// {
+//     if (n == 1)
+//     {
+//         Console.WriteLine($"Your composition is 1");
+//     }
+//     else 
+//     {
+//         double res = 1;
+//         for (int i=1; i<=n; i++)
+//         {
+//            res *= i; 
+//         }
+//          Console.WriteLine($"Your composition is {res}");
+//     }
+// }
+// Console.WriteLine("Please enter your number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Fact(num);
+
+// Напишите программу, которая находит в последовательности
+// от 1 до N все нечетные числа
+
+// Напишите программу, которая находит 
+// в последовательности от 1 до N все нечетные числа
+// void OddEven(int num){
+//     for(int i = 1; i<=num; i+=2){
+//         Console.Write(i + " ");
+//     }
+//     Console.WriteLine();
+//     for(int j = 2; j<=num; j+=2){
+//         Console.Write( j + " ");
+//     }
+// }
+
+// Console.WriteLine("Please enter the number:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// OddEven(num);
+
+// Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// int [] CreateRandomArray()
+// {
+//     int[] array = new int [8];
+//     for (int i = 0; i<8; i++)
+//     {
+//         array [i] = new Random(). Next(0,2);
+//     }
+//     return array;
+// }
+
+// void ShowArray (int [] array)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i<array.Length; i++)
+//     {
+//     Console.Write(array[i]+" ");
+//     }
+//     Console.Write("]");
+// }
+
+// ShowArray(CreateRandomArray());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
