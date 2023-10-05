c0172717644b708007c46ced0106e6e17b173e1e
Andrusha <andruwaeinne1@gmail.com>
2023-10-05 16:41:03 +0700

    Seminar5

---
+++ Seminar5OdnomernieMassivi/Program.cs
@@ -0,0 +1,255 @@
+﻿//Задайте массив из 12 элементов, заполненный
+// случайными числами из промежутка [-9, 9]. Найдите сумму
+// отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
+// положительных чисел равна 29, сумма отрицательных равна -20.
+
+// int [] CreateRandomArray(int min, int max, int size)
+// {
+//     int [] array = new int [size]; // временное хранилище массива. Сайз от пользователя
+//     for (int i=0; i<size; i++)
+//     {
+//         array[i] = new Random().Next(min,max+1);
+//     }
+//     return array;
+// }
+
+// void ShowArray(int [] array)
+// {
+//     for (int i = 0; i<array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void ClacPosNeg (int [] array)
+// {
+//     int sumPos = 0;
+//     int sumNeg = 0;
+//     for (int i = 0; i<array.Length; i++)
+//     {
+//         if (array[i]<1)
+//         {
+//             sumNeg +=array[i];
+//         }
+//         else 
+//         {
+//             sumPos +=array[i];
+//         }
+//     }
+//     Console.WriteLine($"Summ of neg is {sumNeg} and summ positiv is {sumPos}");
+// }
+
+// Console.WriteLine("Enter min of array value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter max of array value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter size of array: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// int [] newArray = CreateRandomArray(min,max,size);
+// ShowArray(newArray);
+// ClacPosNeg(newArray);
+
+
+//Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// int [] CreateRandomArray(int min, int max, int size)
+// {
+//     int [] array = new int [size]; // временное хранилище массива. Сайз от пользователя
+//     for (int i=0; i<size; i++)
+//     {
+//         array[i] = new Random().Next(min,max+1);
+//     }
+//     return array;
+// }
+
+// void ShowArray(int [] array)
+// {
+//     for (int i = 0; i<array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void ReversArray (int[] array)
+// {
+//     for (int i = 0; i<array.Length; i++)
+//     {
+//           array[i]*=(-1);
+//     }
+//     ShowArray(array);
+// }
+// Console.WriteLine("Enter min of array value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter max of array value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter size of array: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// int [] newArray = CreateRandomArray(min,max,size);
+// ShowArray(newArray);
+// ReversArray(newArray);
+
+
+// Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// int [] CreateRandomArray(int min, int max, int size)
+// {
+//     int [] array = new int [size]; // временное хранилище массива. Сайз от пользователя
+//     for (int i=0; i<size; i++)
+//     {
+//         array[i] = new Random().Next(min,max+1);
+//     }
+//     return array;
+// }
+
+// void ShowArray(int [] array)
+// {
+//     for (int i = 0; i<array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// bool IsPresent(int[] array, int num){
+//     for(int i = 0; i<array.Length; i++){
+//         if(array[i]==num)
+//             return true;
+//     }
+//     return false;
+// }
+
+// Console.WriteLine("Enter min of array value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter max of array value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter size of array: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter numb to search: ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+
+// int [] newArray = CreateRandomArray(min,max,size);
+// ShowArray(newArray);
+// bool flag = IsPresent(newArray, numb);
+// Console.WriteLine($"Element {numb} in array is {flag}");
+
+// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 23 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+// int [] CreateRandomArray(int min, int max, int size)
+// {
+//     int [] array = new int [size]; // временное хранилище массива. Сайз от пользователя
+//     for (int i=0; i<size; i++)
+//     {
+//         array[i] = new Random().Next(min,max+1);
+//     }
+//     return array;
+// }
+
+// void ShowArray(int [] array)
+// {
+//     for (int i = 0; i<array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void ShowSunInRange(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i<array.Length; i++)
+//     {
+//         if (array[i]>9 && array[i]<100)
+//         {
+//             count++;
+//         }
+//     }
+//     Console.WriteLine($"Count of number in range [10,99] is {count}");
+// }
+
+
+// Console.WriteLine("Enter min of array value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter max of array value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter size of array: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+
+// int [] newArray = CreateRandomArray(min,max,size);
+// ShowArray(newArray);
+// ShowSunInRange(newArray);
+
+
+//: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 12 10
+
+
+int [] CreateRandomArray(int min, int max, int size)
+{
+    int [] array = new int [size]; // временное хранилище массива. Сайз от пользователя
+    for (int i=0; i<size; i++)
+    {
+        array[i] = new Random().Next(min,max+1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array)
+{
+    for (int i = 0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void Multi(int [] array)
+{
+    int size = 0;
+    if (array.Length %2 ==0)
+        size = array.Length /2;
+    else
+        size = (array.Length+1)/2;
+    int [] arrayMult = new int [size];
+    for (int i=0; i<array.Length/2; i++)
+    {
+        arrayMult[i] = array[i] * array[array.Length-1-i];
+    }
+    if (array.Length%2 != 0)
+        arrayMult[size-1] = array[size-1];
+    ShowArray(arrayMult);
+}
+
+
+Console.WriteLine("Enter min of array value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter max of array value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+
+int [] newArray = CreateRandomArray(min,max,size);
+ShowArray(newArray);
+Multi(newArray);
